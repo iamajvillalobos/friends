a25055ca071b8623ea363645ac193a954d8b6eb4
Arman Jon Villalobos <me@armanjon.com>
2017-09-29 22:27:20 +0800

    Setup ecto database

--- lib/friends/application.ex
+++ lib/friends/application.ex
@@ -6,10 +6,8 @@ defmodule Friends.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Friends.Worker.start_link(arg)
-      # {Friends.Worker, arg},
+      Friends.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
